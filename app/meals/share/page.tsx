366921683bd295ede675d85d5a786d005f800fd1
Zafeer Hafeez <contact@zafeer.pk>
2024-09-13 15:26:34 +0500

    feat: share meal page is responsive

--- app/meals/share/page.tsx
+++ app/meals/share/page.tsx
@@ -12,12 +12,12 @@ import { useFormState } from "react-dom";
 export default function ShareMealPage() {
   const [state, formAction] = useFormState(shareMeal, { message: "" });
 
-  return <form className="max-w-screen-md mx-auto flex flex-col gap-8 my-16" action={formAction}>
+  return <form className="max-w-screen-md mx-auto flex flex-col gap-4 md:gap-8 my-16 px-10" action={formAction}>
     <header className="mb-12">
       <h1 className="text-6xl font-semibold mb-2">Share your <HighLightedText>favorite recipe</HighLightedText></h1>
       <p className="text-2xl font-light">The world is waiting for your delicious recipe!</p>
     </header>
-    <div className="flex flex-row gap-8">
+    <div className="flex flex-col md:flex-row gap-4 md:gap-8">
       <TextInput label="Your name" name="name" />
       <TextInput label="Email" name="email" type="email" />
     </div>
