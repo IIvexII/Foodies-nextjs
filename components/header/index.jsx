0ec8674e221024b901e8bcfff1e8d214ad612a82
Zafeer Hafeez <contact@zafeer.pk>
2024-09-13 14:06:50 +0500

    feat: make header responsive

--- components/header/index.jsx
+++ components/header/index.jsx
@@ -9,19 +9,19 @@ export default function Header() {
   return (
     <>
       <HeaderBackground />
-      <header className="mx-20 my-8 flex items-center justify-between">
+      <header className="mx-6 my-8 flex flex-col items-center justify-between gap-6 md:mx-20 md:flex-row">
         <Link href="/" className="flex items-center gap-4">
           <Image
             src={Logo}
-            width={80}
-            height={80}
+            fill
             alt="Image of a tasty food which is logo of this website"
             priority
+            className="!relative !h-14 !w-14"
           />
-          <h1 className="text-4xl font-bold">Foodies</h1>
+          <h1 className="text-3xl font-bold lg:text-4xl">Foodies</h1>
         </Link>
 
-        <nav className="mr-32 flex gap-16 text-xl">
+        <nav className="mr-0 flex overflow-clip rounded-full bg-gradient-to-br from-orange-600 via-orange-500 to-orange-400 text-sm md:gap-8 md:overflow-hidden md:bg-none md:text-lg lg:mr-32 lg:gap-12 lg:text-xl">
           <NavLink href="/">Home</NavLink>
           <NavLink href="/meals">Browse Meals</NavLink>
           <NavLink href="/community">Foodies Community</NavLink>
--- components/header/nav-link.tsx
+++ components/header/nav-link.tsx
@@ -15,7 +15,7 @@ export default function NavLink({ href, className, children }: NavLinkProps) {
   return (
     <Link
       href={href || ""}
-      className={`font-semibold tracking-wide hover:text-orange-300 hover:[text-shadow:4px_4px_10px_rgba(255,165,0,0.8)] ${className} ${path === href ? "text-orange-300 [text-shadow:4px_4px_10px_rgba(255,165,0,0.8)]" : ""}`}
+      className={`font-semibold tracking-wide px-3 py-3 md:p-0 hover:bg-black/15 md:hover:bg-transparent md:hover:text-orange-300 md:hover:[text-shadow:2px_2px_6px_rgba(255,165,0,0.8)] ${className} ${path === href ? "bg-black/15 md:bg-transparent md:text-orange-300 md:[text-shadow:2px_2px_6px_rgba(255,165,0,0.8)]" : ""}`}
     >
       {children}
     </Link>
