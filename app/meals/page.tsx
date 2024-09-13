4a4de057c233c5e826e8999856a764e0847f4c6b
Zafeer Hafeez <contact@zafeer.pk>
2024-09-13 15:04:11 +0500

    feat: all meals page is responsive

--- app/meals/page.tsx
+++ app/meals/page.tsx
@@ -10,20 +10,20 @@ import { getMeals } from "@/db/queries/meals";
 async function ShowMeals() {
   const meals = await getMeals();
 
-  return <MealGrid className="px-16" meals={meals} />;
+  return <MealGrid className="px-10 md:px-16 " meals={meals} />;
 }
 
 export default function MealsPage() {
   return (
     <>
-      <header className="mt-16 flex flex-col gap-6 px-16">
-        <h1 className="text-6xl font-semibold">
+      <header className="mt-16 flex flex-col gap-0 md:gap-6 px-10 md:px-16">
+        <h1 className="text-4xl md:text-6xl font-semibold">
           Delicious meeals, created <HighLightedText>by you</HighLightedText>
         </h1>
-        <p className="text-2xl font-light">
+        <p className="text-xl md:text-2xl font-light">
           Choose your favorite and cook it is easy and fun!
         </p>
-        <CTAButton className="place-self-start" href="/meals/share">
+        <CTAButton className="place-self-start mt-8 md:mt-0" href="/meals/share">
           Share Your Favorite Recipe
         </CTAButton>
       </header>
--- components/image/meal-grid.tsx
+++ components/image/meal-grid.tsx
@@ -25,22 +25,21 @@ export default async function MealGrid({ meals, className }: MealGridProps) {
   )
 
   return (
-    <div className={`mt-16 grid grid-cols-3 gap-8 ${className}`}>
+    <div className={`mt-16 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-stretch gap-8 ${className}`}>
       {mealsWithPlaceholders.map((meal) => {
         return (
           <div
             key={meal.id}
-            className="flex max-h-[560px] flex-col rounded-lg bg-gradient-to-tr from-red-950 to-red-900 shadow-lg"
+            className="flex h-full flex-col rounded-lg bg-gradient-to-tr from-red-950 to-red-900 shadow-lg"
           >
             <div className="h-1/2">
               <Image
                 src={meal?.image}
                 alt={meal?.title}
-                width={400}
-                height={200}
+                fill
                 blurDataURL={meal.placeholder}
                 placeholder="blur"
-                className="h-full rounded-t-lg object-cover"
+                className="!relative w-full h-full rounded-t-lg object-cover"
               />
             </div>
             <div className="flex h-full flex-col justify-between gap-6 rounded-b-lg bg-black bg-opacity-70 px-6 pb-6 pt-4 backdrop-blur-xl">
